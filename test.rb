09e9e9c6c216f4f4c4cc2d52a87e22f63bf7d34f
ryo maruyama <t.t.uncle0803@gmail.com>
2021-05-28 17:04:00 +0900

    kadai

---
+++ test.rb
@@ -0,0 +1,18 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+# この一行を追記
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'kadai.html.erb')
+server.mount('/goya_false.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_false.rb')
+
+server.start
